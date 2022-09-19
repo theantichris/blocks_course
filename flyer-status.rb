9afb68faf75fe4226422a2891a6753c5fe5f12c6
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-19 09:53:54 -0400

    starting with new Flyer class

---
+++ flyer-status.rb
@@ -0,0 +1,21 @@
+class Flyer
+  attr_reader :name, :email, :miles_flown
+  attr_accessor :status
+
+  def initialize(name, email, miles_flown, status = :bronze)
+    @name = name
+    @email = email
+    @miles_flown = miles_flown
+    @status = status
+  end
+
+  def to_s
+    "#{name} (#{email}): #{miles_flown} - #{status}"
+  end
+end
+
+flyers = []
+flyers << Flyer.new("Larry", "larry@example.com", 4000, :platinum)
+flyers << Flyer.new("Moe", "moe@example.com", 1000)
+flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
+flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
