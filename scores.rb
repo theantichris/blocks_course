9082b54d90a2dce02b3ccb322915465523e10dbd
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-20 10:44:32 -0400

    playing around with enumerables

--- scores.rb
+++ scores.rb
@@ -1,5 +1,19 @@
 scores = [83, 71, 92, 64, 98, 87, 75, 69]
 
-scores.select! { |score| score > 70 }
-scores.reject! { |score| score.even? }
-p scores
+puts "Odd scores over 70:"
+overSeventy = scores.select { |score| score > 70 }
+oddsOverSeventy = overSeventy.reject { |score| score.even? }
+p oddsOverSeventy
+
+puts "\nScores doubled:"
+scores_doubled = scores.map { |score| score *2 }
+p scores_doubled
+
+puts "\nSum:"
+sum = scores.reduce(:+)
+p sum
+
+puts "\nEvens and odds"
+evens, odds = scores.partition { |score| score.even? }
+p evens
+p odds
