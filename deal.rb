8d6943007168c3e6acba023478a159cda074d4fe
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-20 13:39:33 -0400

    added dealing cards an N number of times

--- deal.rb
+++ deal.rb
@@ -1,22 +1,25 @@
-def deal
+def deal(number)
   faces = ["Jack", "Queen", "King", "Ace"]
   suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 
-  random_face = faces.sample
-  random_suit = suits.sample
+
 
   if block_given?
-    score = yield(random_face, random_suit)
-    puts "You scored a #{score}!"
+    1.upto(number) do
+      random_face = faces.sample
+      random_suit = suits.sample
+
+      score = yield(random_face, random_suit)
+
+      puts "You scored a #{score}!"
+    end
   else
     puts "No deal!"
   end
 end
 
-deal do |face, suit|
+deal(10) do |face, suit|
   puts "Dealt a #{face} of #{suit}"
 
   face.length + suit.length
 end
-
-deal
