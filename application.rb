29c8883442a1200bae0bc66038bbb64416026131
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-23 08:30:06 -0400

    switching application environment with yield

---
+++ application.rb
@@ -0,0 +1,42 @@
+class Application
+  attr_accessor :environment
+
+  def initialize
+    @environment = :development
+  end
+
+  def connect_to_database
+    puts "Connecting to #{@environment} database..."
+  end
+
+  def handle_request
+    puts "Handling #{@environment} request..."
+  end
+
+  def write_to_log
+    puts "Writing to #{@environment} log file..."
+  end
+
+  def in_production
+    old_environment = @environment
+    @environment = :production
+
+    yield
+
+    @environment = old_environment
+    puts "Reset environment to #{@environment}."
+  end
+end
+
+app = Application.new
+app.connect_to_database
+app.handle_request
+app.write_to_log
+
+puts "\n\n"
+
+app.in_production do
+  app.connect_to_database
+  app.handle_request
+  app.write_to_log
+end
